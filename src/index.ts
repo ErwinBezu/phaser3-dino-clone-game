2268c5976976bd3ae87e803320fa0424e7e6a2f8
Erwin Bezu <erwin.bezu@gmail.com>
2024-04-12 15:22:54 +0200

    finish tutorial

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,26 @@
 
 import Phaser from "phaser";
+import PreloadScene from "./scenes/PreloadScene";
+import PlayScene from "./scenes/PlayScene";
+
+export const PRELOAD_CONFIG = {
+  cactusesCount: 6,
+  birdsCount: 1
+}
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: 1000,
+  height: 340,
+  pixelArt: true,
+  transparent: true,
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 200 }
+      debug: false,
     }
   },
-  scene: {
-    preload: preload,
-    create: create
-  }
+  scene: [PreloadScene, PlayScene]
 };
 
 new Phaser.Game(config);
-
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
-}
-
-function create () {
-  this.add.image(400, 300, 'sky');
-}
